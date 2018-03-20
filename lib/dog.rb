7aedf64112c226af695510dbf3f93e5025b1e166
Alex McKean <amckean@me.com>
2018-03-20 13:50:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,5 +33,11 @@ class Dog
     self.new(id: id , name: name , breed: breed)
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+
 
 end
